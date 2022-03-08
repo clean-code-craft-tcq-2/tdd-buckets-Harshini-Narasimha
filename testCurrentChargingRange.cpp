98e393f065e05d432d41c319b0e0d5b0d27d0919
Harshini Narasimha <91076579+Harshini-Narasimha@users.noreply.github.com>
2022-03-09 00:23:20 +0530

    Update testCurrentChargingRange.cpp

--- testCurrentChargingRange.cpp
+++ testCurrentChargingRange.cpp
@@ -4,6 +4,20 @@
 
 TEST_CASE("Output the number of readings where the current measurements have 2 continuous current charging data") {
   std::vector<int> chargingCurrentSampleList = {4,5};
-  std::map<std::string, int> expectedOutputRange = {{"4-5", 2}};
+  std::map<std::string, int> expectedOutputRange = {{"4-5",2}};
   REQUIRE(getCurrentReadingsFromRanges(chargingCurrentSampleList) == expectedOutputRange);
 }
+
+TEST_CASE("Output the number of readings where the current measurements have 1 current charging data") {
+  std::vector<int> chargingCurrentSampleList = {4};
+  std::map<std::string, int> expectedOutputRange = {{"4-4",1}};
+  REQUIRE(getCurrentReadingsFromRanges(chargingCurrentSampleList) == expectedOutputRange);
+}
+  
+TEST_CASE("Output the number of readings where the current measurements have multiple current charging data") {
+  std::vector<int> chargingCurrentSampleList = {5,4,5,9,10,11,12,13};
+  std::map<std::string, int> expectedOutputRange = {{"4-5",3},{"9-13",5}};
+  REQUIRE(getCurrentReadingsFromRanges(chargingCurrentSampleList) == expectedOutputRange);
+}
+
+  
