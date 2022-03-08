9dc2dbb9e71e88b2555feb59fa69208b080e3025
Harshini Narasimha <91076579+Harshini-Narasimha@users.noreply.github.com>
2022-03-09 00:37:30 +0530

    Update chargingCurrentRangeCreator.h

--- chargingCurrentRangeCreator.h
+++ chargingCurrentRangeCreator.h
@@ -6,8 +6,6 @@ using namespace std;
 typedef map<string, int> currentRangeListWithReadings;
 typedef vector<int> chargingCurrentMeasurementList;
 
-std::string formatCurrentRangeString(int lowerRange, int upperRange);
-bool isGivenReadingsContinous(int lowerRange, int upperRange);
 currentRangeListWithReadings getRangeWithSingleReading(int chargingCurrentValue);
 currentRangeListWithReadings getRangeWithMultipleReading(chargingCurrentMeasurementList currentMeasurementList);
 currentRangeListWithReadings getCurrentReadingsFromRanges(chargingCurrentMeasurementList & currentMeasurementList);
