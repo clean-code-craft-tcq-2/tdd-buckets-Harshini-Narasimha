2cef3ce17f288f9b65c5fb9e6200ace61d48a834
Harshini Narasimha <91076579+Harshini-Narasimha@users.noreply.github.com>
2022-03-29 23:33:59 +0530

    Update chargingCurrentRangeCreator.h

--- chargingCurrentRangeCreator.h
+++ chargingCurrentRangeCreator.h
@@ -10,7 +10,7 @@ string formatCurrentRangeString(int lowerRange, int upperRange);
 bool isGivenReadingsContinous(int lowerRange, int upperRange);
 currentRangeListWithReadings getRangeWithSingleReading(int chargingCurrentValue);
 currentRangeListWithReadings getRangeWithMultipleReading(chargingCurrentMeasurementList currentMeasurementList);
-currentRangeListWithReadings getCurrentReadingsFromRanges(chargingCurrentMeasurementList & currentMeasurementList);
+currentRangeListWithReadings getCurrentReadingsFromRanges(const chargingCurrentMeasurementList & currentMeasurementList);
 bool isChargingCurrentMeasurementListValid(chargingCurrentMeasurementList currentMeasurementList);
 int ConvertA2DToAmps(int ADCValue);
 std::vector<int> getConvertedAmpsList(const std::vector<int> 12BitADCList);
