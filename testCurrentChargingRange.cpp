5e8f81c2f7eac9351ec8c98d89a49e6cd69eeba8
Harshini Narasimha <91076579+Harshini-Narasimha@users.noreply.github.com>
2022-03-29 23:20:38 +0530

    Update testCurrentChargingRange.cpp

--- testCurrentChargingRange.cpp
+++ testCurrentChargingRange.cpp
@@ -26,31 +26,31 @@ TEST_CASE("Output the number of readings where the current measurements have 2 c
   REQUIRE(getCurrentReadingsFromRanges(chargingCurrentSampleList) == expectedOutputRange);
 }
 
-TEST_CASE("Range list emoty when no reading available") {
+TEST_CASE("Range list empty when no reading available") {
   std::vector<int> emptyChargingCurrentSampleList;
   std::map<std::string, int> expectedEmptyOutputRange;
   REQUIRE(getCurrentReadingsFromRanges(emptyChargingCurrentSampleList) == expectedEmptyOutputRange);
 }
 
 TEST_CASE("Output the number of readings where the current measurements have 1 current charging data") {
-  std::vector<int> chargingCurrentSampleList = {4};
+  std::vector<int> chargingCurrentSampleList = {1546};
   std::map<std::string, int> expectedOutputRange = {{"4-4",1}};
   REQUIRE(getCurrentReadingsFromRanges(chargingCurrentSampleList) == expectedOutputRange);
 }
   
 TEST_CASE("Output the number of readings where the current measurements have multiple current charging data") {
-  std::vector<int> chargingCurrentSampleList = {5,4,5,9,10,11,12,13};
-  std::map<std::string, int> expectedOutputRange = {{"4-5",3},{"9-13",5}};
+  std::vector<int> chargingCurrentSampleList = {1846,1546,1946,3000,3400,3500,4094};
+  std::map<std::string, int> expectedOutputRange = {{"4-5",3},{"7-10",4}};
   REQUIRE(getCurrentReadingsFromRanges(chargingCurrentSampleList) == expectedOutputRange);
 }
 
-TEST_CASE("Get the Reading for list with single entry") {
-  std::map<std::string, int> expectedOutputRange = {{"7-7",1}};
-  REQUIRE(getRangeWithSingleReading(7) == expectedOutputRange);
+TEST_CASE("Get the Reading for list with discontinuous entry") {
+  std::map<std::string, int> expectedOutputRange = {{"7-10",4}};
+  REQUIRE(getRangeWithSingleReading(3400,3000,4094,3500) == expectedOutputRange);
 }
 
 TEST_CASE("Get the Reading for list with multiple entry") {
-  std::vector<int> chargingCurrentSampleList = {4,7,10};
+  std::vector<int> chargingCurrentSampleList = {1546,3000,4092};
   std::map<std::string, int> expectedOutputRange = {{"4-4",1},{"7-7",1},{"10-10",1}};
   REQUIRE(getCurrentReadingsFromRanges(chargingCurrentSampleList) == expectedOutputRange);
 }
